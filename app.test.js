b8fc9f30fac8000380159097095ee1a90cec95a8
faiz-dev <68036226+faiz-dev@users.noreply.github.com>
2024-10-24 14:41:46 +0700

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -2,8 +2,8 @@ const main = require('./app')
 
 describe('Angka Dadu Raka', () => { 
   it('Menampilkan dadu Ganjil dan Genap', () => {
-    main()
     const consoleSpy = jest.spyOn(console, 'log')
+    main()
 
     expect(consoleSpy).toHaveBeenCalledWith('Raka mendapatkan angka ganjil sebanyak 3 kali dan angka genap sebanyak 3 kali')
   })
